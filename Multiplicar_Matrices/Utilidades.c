a80434f9b4f03d2284d8505dc49d35e9d0226a53
Bruno A Parisi <bnoprs00@gmail.com>
2024-11-30 04:31:45 -0300

    elime archivos obsoletos

--- Multiplicar_Matrices/Utilidades.c
+++
@@ -1,6 +0,0 @@
-#include "Utilidades.h"
-#include <stdio.h>
-
-int checkNull(void *ptr) {
-    return (ptr == NULL) ? 0 : 1;
-}
--- Multiplicar_Matrices/Utilidades.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILIDADES_H_INCLUDED
-#define UTILIDADES_H_INCLUDED
-
-int checkNull(void *ptr);
-
-#endif // UTILIDADES_H_INCLUDED
